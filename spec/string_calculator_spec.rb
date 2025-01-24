fd39133b1acc0bd20069c276382863f18a997074
Preetha S <preetha.prakash0807@gmail.com>
2025-01-25 03:10:51 +0530

    handles newlines as delimiters

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -19,4 +19,8 @@ RSpec.describe StringCalculator do
     expect(StringCalculator.add("1,2,3,4")).to eq(10)
     expect(StringCalculator.add("1,2,3,4,5")).to eq(15)
   end
+
+  it 'handles newlines as delimiters' do
+    expect(StringCalculator.add("1\n2,3")).to eq(6)
+  end
 end
