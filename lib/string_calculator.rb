42b354ca883503c9f4c2add1d726a27850c6afbf
Preetha S <preetha.prakash0807@gmail.com>
2025-01-25 12:53:50 +0530

    Optimizes delimiter logic

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,12 +2,15 @@ class StringCalculator
   def self.add(numbers)
     return 0 if numbers.empty?
 
+    delimiters = [",", "\n"]
+
     if numbers.start_with?("//")
-      delimiter, numbers = numbers[2..].split("\n", 2)
-      nums = numbers.split(delimiter).map(&:to_i)
-    else
-      nums = numbers.split(/,|\n/).map(&:to_i)
+      custom_delimiters, numbers = numbers[2..].split("\n", 2)
+      delimiters << custom_delimiters
     end
+
+    delimiter_regex = Regexp.union(delimiters)
+    nums = numbers.split(delimiter_regex).map(&:to_i)
 
     negatives = nums.select { |n| n.negative? }
     raise "Negatives are not allowed: #{negatives.join(', ')}" unless negatives.empty?
